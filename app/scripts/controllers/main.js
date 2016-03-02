c3e86998e1f481afbb42f9da78969dcccf7a1ccb
Mariangel <mariangel.gfarias@gmail.com>
2016-03-01 22:33:09 -0600

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cualestucorreoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cualestucorreoApp
+ */
+angular.module('cualestucorreoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
